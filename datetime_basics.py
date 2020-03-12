b44dd0e3501bd75127a3d7759e33bbefe1a58402
bakunobu <bakunobu@gmail.com>
2020-03-12 12:15:33 +0300

    starting point

---
+++ datetime_basics.py
@@ -0,0 +1,11 @@
+import datetime
+
+date_1 = datetime.datetime.now()
+print(date_1)
+print(date_1.__repr__())
+print(type(date_1))
+
+date_2 = datetime.date.today()
+print(date_2)
+print(date_2.__repr__())
+print(type(date_2))
